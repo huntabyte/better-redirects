9481870eb70a39534a268657b81f66e857d38b5b
Hunter Johnston <johnstonhuntera@gmail.com>
2023-03-27 21:15:03 -0400

    done

--- src/routes/account/+page.server.ts
+++ src/routes/account/+page.server.ts
@@ -3,6 +3,6 @@ import { redirect } from "@sveltejs/kit"
 
 export const load = async (event) => {
 	if (!event.locals.user) {
-		throw redirect(302, handleLoginRedirect(event, "You are not allowed to access!"))
+		throw redirect(302, handleLoginRedirect(event))
 	}
 }
